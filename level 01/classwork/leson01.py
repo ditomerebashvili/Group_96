c55a670976bae86f9988f41b1eef394ea46fbfc5
ditomerebashvili <ditomerebashvili85@gmail.com>
2026-02-05 20:15:26 +0400

    second commit

---
+++ level 01/classwork/leson01.py
@@ -0,0 +1,15 @@
+name = ("dito")
+print(name)   #string გამოვიყენეთ ტექსტური სახის მნიშვნელობა რომელსაც სჭირდება ბრჭყალები
+
+
+surname = ("merebashvili")
+print(surname)   #string
+
+
+age = ("15")
+print(age) #integer გამოვიყენეთ რიცხვითი მნიშვნელობა
+
+
+
+height = (1.75)
+print(height)
