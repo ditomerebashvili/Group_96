41d5f15b9486f056cf025434129f1318efd7d97d
ditomerebashvili <ditomerebashvili85@gmail.com>
2026-02-16 20:35:03 +0400

    second commit

---
+++ level 04/classwork/classwork.py
@@ -0,0 +1,9 @@
+user_1 = "Nika"  #ცვლადში პირველი არ უნდა იყოს რიცხვიტი მნიშვნელობა ამიტომ user ს და 1 ს ადგილები შევუცვალეთ
+user_2 = "Nika" #აქაც იგივე ნაირად
+
+print(user_2) #აქ ცვლადის სახელი გადმოვიტანეთ იდენტურად რადგან სხვა შემტხვევაში ცვლადში შენახულ ინფორმაცია ტერმინალზე ვერ გამოიტანდა
+
+name = input("enter your name:")
+surname = input("enter your surname: ")
+adress = input("enter your address: ")
+age = input("enter your age: ")
