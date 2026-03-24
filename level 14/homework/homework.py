eb0b5fe6cf8abf2130a5b075b5d1d86cb3c5809f
ditomerebashvili <ditomerebashvili85@gmail.com>
2026-03-25 00:11:00 +0400

    second commit

---
+++ level 14/homework/homework.py
@@ -0,0 +1,24 @@
+#1 დავალება
+phrase = "python programming"
+print(phrase[0:6])
+print(phrase[7:])
+print(phrase[0::3])
+
+#2 დავალება
+text = "PythonSlicing"
+print(text[:6])
+print(text[::-1])
+print(text[::2])
+
+#3 დავალება 
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+print(letters[::2])
+print(letters[2:7:2])
+print(letters[:-6:-1])
+
+
+#4 დავალება
+sentence = "Slicing makes Python powerful"
+print(sentence[14:20])
+print(sentence[21:][::-1])
+print(sentence[::3])
